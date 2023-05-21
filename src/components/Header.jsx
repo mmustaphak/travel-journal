291bf79420edb3b3249620a5af084725d85e2d1c
Mohammed Mustapha Kura <mustykmohammed@gmail.com>
2023-05-21 11:05:35 +0100

    Added Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css"
+export default function Header(){
+    return(
+        <header>
+            <img src="src/assets/globe.svg"/>
+            <p>my travel journal.</p>
+        </header>
+    )
+}
